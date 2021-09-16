f92aecb746270626e1220c513c6833e5161097e2
Anik Patel <74193405+Bobingstern@users.noreply.github.com>
2021-09-16 07:51:43 -0400

    Create pGraph.js

---
+++ pGraph.js
@@ -0,0 +1,60 @@
+class pGraph{
+  
+  constructor(x1, y1, w, h){
+    this.left = createVector(x1, y1)
+    this.size = createVector(w, h)
+    this.inc;
+    this.points = []
+    this.greatestY = 0;
+  }
+  show(){
+    fill(255)
+    rect(this.left.x, this.left.y, this.size.x, this.size.y)
+    if (this.points.length > 0){
+      this.inc = this.size.x/this.points.length
+      for (var i=0;i<this.points.length;i++){
+        push()
+        translate(this.left.x, this.left.y)
+        
+        circle(i*this.inc, map(this.points[i], 0, this.greatestY, 0, this.size.y), 1)
+        if (i > 0){
+          line(i*this.inc, map(this.points[i], 0, this.greatestY, 0, this.size.y), (i-1)*this.inc, map(this.points[i-1], 0, this.greatestY, 0, this.size.y))
+        }
+        pop()
+        
+        
+      }
+    }
+  }
+  add(y){
+
+    if (y > this.greatestY){
+      this.greatestY = y
+    }
+    if (y <= 0){
+      y = 0.01;
+    }
+    this.points.push(y)
+  }
+  addArray(arr){
+    for (var i=0;i<arr.length;i++){
+      this.add(arr[i])
+    }
+  }
+  removeArray(rr){
+    for (var i=0;i<arr.length;i++){
+      if (arr[i] >= 0 && arr[i] < this.points.length-1){
+        this.remove(arr[i])
+      }
+      
+    }
+  }
+  getLength(){
+    return this.points.length
+  }
+  
+  
+  remove(i){
+    this.points.splice(i, 1)
+  }
+}
